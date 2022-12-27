003f0d107430c9aa9b28bc9a0b0f089d0ed1e43f
kk-2503 <alu0101109046@ull.edu.es>
2022-12-27 19:56:17 +0000

    act 2 part 1

--- act2.py
+++ act2.py
@@ -1,5 +1,7 @@
 import os
 import psycopg2
+from flask import Flask
+app = Flask(__name__)
 
 def get_db_conn():
   try:
@@ -14,22 +16,28 @@ def get_db_conn():
   except (Exception, psycopg2.Error) as error :
     print ("Error while connecting to PostgreSQL: ", error)
 
-def api1():
+@app.route('/<int:number>/')
+def api(number):
     # Retorno de la temperatura media de todas las habitaciones.
     conn = get_db_conn()
     cur = conn.cursor()
-    try:
-      cur.execute('SELECT AVG(temperature) AS avg_temp, room_id'
-                  ' FROM temperatures GROUP BY(room_id) ORDER BY(avg_temp) DESC;')
-    except (Exception, psycopg2.Error) as error :
-      print ("Error while performing API #1: ", error)
-      pass
+    if (number == 1):
+      try:
+        cur.execute('SELECT AVG(temperature) AS avg_temp, room_id'
+                    ' FROM temperatures GROUP BY(room_id) ORDER BY(avg_temp) DESC;')
+      except (Exception, psycopg2.Error) as error :
+        print ("Error while performing API #1: ", error)
+        pass
 
-    print("Avg Temp\tRoom ID")
-    for x in cur.fetchall():
-      print("{} \t {}".format(round(x[0], 2), x[1]))
+      retValue = "Avg Temp\tRoom ID <br /> "
+      for x in cur.fetchall():
+        retValue += "{} \t {} <br /> ".format(round(x[0], 2), x[1])
+
+      return retValue
     cur.close()
     conn.close()
 
-api1()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
     
